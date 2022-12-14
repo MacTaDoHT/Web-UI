dfaa288f54f4fc3eb2e750c455524b9298279c42
MacTaDoHT <zarinzgame@gmail.com>
2022-12-14 12:29:17 +0300

    Урок 3. Основы Selenium

---
+++ src/main/java/org/example/dz06/CreateCrmProjectExample/AllMenuProjects.java
@@ -0,0 +1,24 @@
+package org.example.dz06.CreateCrmProjectExample;
+
+import org.example.dz06.BaseView;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class AllMenuProjects extends BaseView {
+    public AllMenuProjects(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(xpath = "//span[text()='Все проекты']")
+    public WebElement allMenuProjectsItem;
+
+    public void goAllMenuProjects() {
+
+        allMenuProjectsItem.click();
+        webDriverWait.until(
+                ExpectedConditions.presenceOfElementLocated(By.xpath(CreateAProject.createXpathLocator)));
+    }
+}
---
+++ src/main/java/org/example/dz06/CreatingCrmContactPerson/PageOfContactCreation.java
@@ -0,0 +1,45 @@
+package org.example.dz06.CreatingCrmContactPerson;
+
+import org.example.dz06.BaseView;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class PageOfContactCreation extends BaseView {
+    public PageOfContactCreation(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(name = "crm_contact[lastName]")
+    public WebElement lastName;
+
+    public PageOfContactCreation crmLastName(String name) {
+        lastName.sendKeys(name);
+        return this;
+    }
+
+    @FindBy(name = "crm_contact[firstName]")
+    public WebElement firstName;
+
+    public PageOfContactCreation crmFirstName(String name) {
+        firstName.sendKeys(name);
+        return this;
+    }
+
+    @FindBy(name = "crm_contact[jobTitle]")
+    public WebElement jobTitle;
+
+    public PageOfContactCreation crmJobTitle(String name) {
+        jobTitle.sendKeys(name);
+        return this;
+    }
+
+    @FindBy(xpath = saveAndCloseButtonXpathLocator)
+    public WebElement saveAndCloseButton;
+
+    public static final String saveAndCloseButtonXpathLocator = "//button[contains(text(), 'Сохранить и закрыть')]";
+
+    @FindBy(xpath = "//*[text()='Значение не должно быть пустым.']")
+    public WebElement requestSaveOneElement;
+
+}
